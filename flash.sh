f765bcfe1488af821865a8c86b8f253cca97822f
Franky333 <1.daniel.frank@gmail.com>
2015-11-01 16:19:17 +0100

    fixed shellscript

--- flash.sh
+++ flash.sh
@@ -6,8 +6,7 @@ if [[ $# -eq 0 ]]; then
 fi
 
 if [[ -f $1 ]]; then
-    echo 'sucess'
-    #./stm32loader.py -e -w -v $1
+    ./stm32loader.py -e -w -v $1
 else
     echo "$1 is not a file, exiting!"
     exit 1
