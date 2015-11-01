42ec5aaa82618095ceaef1231a9ecd64c9406733
Franky333 <1.daniel.frank@gmail.com>
2015-11-01 16:08:56 +0100

    improved shell script

--- flash.sh
+++ flash.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-./stm32loader.py -e -w -v $1
+if [[ $# -eq 0 ]]; then
+	echo "No file to flash specified, exiting!"
+	exit 1
+fi
+
+if [[ -f $1 ]]; then
+    echo 'sucess'
+    #./stm32loader.py -e -w -v $1
+else
+    echo "$1 is not a file, exiting!"
+    exit 1
+fi
+
+
